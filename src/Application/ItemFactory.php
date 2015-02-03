694586bca055f78454b6bc657f53960e3e38cd59
Manel <malotor@gmail.com>
2015-02-03 22:24:37 +0000

    Add id

--- src/Application/ItemFactory.php
+++ src/Application/ItemFactory.php
@@ -2,12 +2,15 @@
 
 namespace malotor\shoppingcart\Application;
 
+use malotor\shoppingcart\Domain\Item;
+
 class ItemFactory {
 
   public static function create($itemName,$itemRef,$itemDescription,$itemPrice) {
     $item = new Item();
 
-    $item->setName($itemName)
+    $item->setId($itemId)
+      ->setName($itemName)
       ->setReference($itemRef)
       ->setDescription($itemDescription)
       ->setPrice($itemPrice);
