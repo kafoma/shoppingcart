61a8873e8a0f7282a0ee5d1b1117ca24084e9627
Manel <malotor@gmail.com>
2015-02-04 22:15:12 +0000

    CartRepository added

---
+++ src/Application/CartRepository.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manel
+ * Date: 03/02/15
+ * Time: 23:31
+ */
+
+namespace malotor\shoppingcart\Application;
+
+use malotor\shoppingcart\Domain\Cart;
+
+class CartRepository {
+  public function __construct($cartLineRepository) {
+    $this->cartLineRepository = $cartLineRepository;
+  }
+  public function get() {
+    $carLines = $this->cartLineRepository->getAll();
+    $cart = new Cart();
+    foreach ($carLines as $carLine) {
+      $cart->addItem($carLine);
+    }
+    return $cart;
+  }
+
+  public function save($cart) {
+    $this->cartLineRepository->removeAll();
+    foreach($cart->getIterator() as $cartLine) {
+      $this->cartLineRepository->save($cartLine);
+    }
+  }
+} 
--- src/Application/Ecommerce.php
+++ src/Application/Ecommerce.php
@@ -2,55 +2,39 @@
 
 namespace malotor\shoppingcart\Application;
 
-use malotor\shoppingcart\Domain\Cart;
-
 class Ecommerce {
   private $itemRepository;
   private $cartRepository;
 
   public function __construct(ItemRepositoryInterface $itemRepository, CartLineRepositoryInterface $cartLineRepository) {
     $this->itemRepository = $itemRepository;
-    $this->cartLineRepository = $cartLineRepository;
+    $this->cartRepository = new CartRepository($cartLineRepository);
   }
 
   public function addProductToCart($productId,$quantity=1) {
     $product = $this->itemRepository->get($productId);
-    $shoppingCart = $this->getCart();
+    $shoppingCart = $this->cartRepository->get();
     $lineCart = CartLineFactory::create($product,$quantity);
     $shoppingCart->addItem($lineCart);
-    $this->saveCart($shoppingCart);
+    $this->cartRepository->save($shoppingCart);
   }
 
   public function removeProductFromCart($productId) {
-    $shoppingCart = $this->getCart();
+    $shoppingCart = $this->cartRepository->get();
     $shoppingCart->removeItem($productId);
-    $this->saveCart($shoppingCart);
+    $this->cartRepository->save($shoppingCart);
   }
 
 
   public function getCartItems() {
-    $cart = $this->getCart();
+    $cart = $this->cartRepository->get();
     return $cart->getIterator();
   }
 
   public function getCartTotalAmunt() {
-    $cart = $this->getCart();
+    $cart = $this->cartRepository->get();
     return $cart->getTotalAmount();
   }
 
-  protected function getCart() {
-    $carLines = $this->cartLineRepository->getAll();
-    $cart = new Cart();
-    foreach ($carLines as $carLine) {
-      $cart->addItem($carLine);
-    }
-    return $cart;
-  }
 
-  protected function saveCart($cart) {
-    $this->cartLineRepository->removeAll();
-    foreach($cart->getIterator() as $cartLine) {
-      $this->cartLineRepository->save($cartLine);
-    }
-  }
 }
